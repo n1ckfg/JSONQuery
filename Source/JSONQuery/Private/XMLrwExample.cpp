756dac29f983c4044ca05544a3ab2400697dfd2c
n1ckfg <nick@fox-gieg.com>
2018-08-30 19:10:27 -0400

    Add xml classes

---
+++ Source/JSONQuery/Private/XMLrwExample.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "XMLrwExample.h"
+
+XMLrwExample::XMLrwExample()
+{
+}
+
+XMLrwExample::~XMLrwExample()
+{
+}
---
+++ Source/JSONQuery/Public/XMLrwExample.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class JSONQUERY_API XMLrwExample
+{
+public:
+	XMLrwExample();
+	~XMLrwExample();
+};
